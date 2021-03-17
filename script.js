94e40bcfbff577f838dbecd2bf996911f5e0e666
Link Tapsell <linkntapsell@hotmail.com>
2021-03-17 19:42:45 +1100

    add pwd response js

--- script.js
+++ script.js
@@ -18,3 +18,49 @@ const SYMBOLS_CODES = arrayFromLowtoHighNum(33,47)
 .concat(arrayFromLowToHighNum(58,64))
 .concat(arrayFromLowToHighNum(91,96))
 .concat(arrayFromLowToHighNum(123,126));
+
+// Password variables 
+var lowerCase = window.confirm("Would you like to use lower case characters?");
+var upperCase = window.confirm("Would you like to use upper case characters?");
+var numbers = window.confirm("Would you like to use numbers?");
+var specialCharacters = window.confirm("Would you like special characters?");
+
+// Password response
+var lowerCaseAnswer = 0;
+if(lowerCase === true){
+    lowerCaseAnswer = true
+} else {
+    lowerCaseAnswer = false
+}
+var upperCaseAnswer = 0;
+if (upperCase === true){
+    upperCaseAnswer = true
+} else { 
+    upperCaseAnswer = false
+}
+var nummberAnswer = 0;
+if(number === true){
+    nummberAnswer = true
+} else { 
+    nummberAnswer = false
+}
+var specialCharactersAnswer = 0;
+if (specialCharacters === true){
+    specialCharactersAnswer = true
+} else {
+    specialCharactersAnswer = false
+}
+let charCode = [];
+if (lowerCaseAnswer == true) {
+    charCodes = charCode.concat(LOWERCASE_CODES);
+}
+if (upperCase == true) {
+    charCodes = charCode.concat(UPPERCASE_CODES);
+}
+if (specialCharacters == true) {
+    charCodes = charCode.concat(SYMBOLS_CODES);
+}
+if (nummberAnswer == true) {
+    charCodes = charCode.concat(NUMBER_CODES);
+}
+
